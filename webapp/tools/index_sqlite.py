1634e3314baa4c647afcef524bac49a097c636f5
ue-sho <tssu45@gmail.com>
2023-11-05 13:15:07 +0900

    create index

---
+++ webapp/tools/index_sqlite.py
@@ -0,0 +1,25 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+
+
+def tenant_db_path(id: int) -> str:
+    """テナントDBのパスを返す"""
+    tenant_db_dir = os.getenv("ISUCON_TENANT_DB_DIR", "../tenant_db")
+    return tenant_db_dir + f"/{id}.db"
+
+
+def connect_to_tenant_db(id: int) -> Engine:
+    """テナントDBに接続する"""
+    path = tenant_db_path(id)
+    engine = create_engine(f"sqlite:///{path}")
+    return engine
+
+
+for i in range(1, 101):
+    tenant_db = connect_to_tenant_db(i)
+    tenant_db.execute("CREATE INDEX IF NOT EXISTS `tenant_id_idx` ON `competition` (`tenant_id`)")
+    tenant_db.execute("CREATE INDEX IF NOT EXISTS `tenant_id_idx` ON `player` (`tenant_id`)")
+    tenant_db.execute("CREATE INDEX IF NOT EXISTS `score_idx` ON `player` (`tenant_id`, `competition_id`, `player_id`)")
+    tenant_db.close()
+
