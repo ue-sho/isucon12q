298abe5f8502e7b7db7d11de5184180b3ad52e99
ue-sho <tssu45@gmail.com>
2023-11-08 00:17:07 +0900

    add index

--- webapp/sql/ctas.sql
+++ webapp/sql/ctas.sql
@@ -6,3 +6,7 @@ DROP TABLE IF EXISTS competition;
 CREATE TABLE player AS SELECT * FROM player_init;
 CREATE TABLE player_score AS SELECT * FROM player_score_init;
 CREATE TABLE competition AS SELECT * FROM competition_init;
+
+CREATE INDEX idx_player_tenant_id ON player (tenant_id);
+CREATE INDEX idx_player_score_ ON player_score_all (tenant_id, competition_id, player_id);
+CREATE INDEX idx_competition_tenant_id ON competition (tenant_id);
