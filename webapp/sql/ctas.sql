b72a60d3e832556aafe00970fb4b66003c0c2bba
ue-sho <tssu45@gmail.com>
2023-11-08 22:21:09 +0900

    create tables

--- webapp/sql/ctas.sql
+++ webapp/sql/ctas.sql
@@ -3,10 +3,40 @@ DROP TABLE IF EXISTS player;
 DROP TABLE IF EXISTS player_score;
 DROP TABLE IF EXISTS competition;
 
-CREATE TABLE player AS SELECT * FROM player_init;
-CREATE TABLE player_score AS SELECT * FROM player_score_init;
-CREATE TABLE competition AS SELECT * FROM competition_init;
+CREATE TABLE `player` (
+    `id` varchar(255) NOT NULL,
+    `tenant_id` bigint NOT NULL,
+    `display_name` text NOT NULL,
+    `is_disqualified` tinyint(1) NOT NULL,
+    `created_at` bigint NOT NULL,
+    `updated_at` bigint NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `idx_player_tenant_id` (`tenant_id`)
+)
 
-CREATE INDEX idx_player_tenant_id ON player (tenant_id);
-CREATE INDEX idx_player_score_all ON player_score (tenant_id, competition_id, player_id);
-CREATE INDEX idx_competition_tenant_id ON competition (tenant_id);
+CREATE TABLE `player_score` (
+    `id` varchar(255) NOT NULL,
+    `tenant_id` bigint NOT NULL,
+    `player_id` varchar(255) NOT NULL,
+    `competition_id` varchar(255) NOT NULL,
+    `score` bigint NOT NULL,
+    `row_num` bigint NOT NULL,
+    `created_at` bigint NOT NULL,
+    `updated_at` bigint NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `idx_player_score_all` (`tenant_id`,`competition_id`,`player_id`)
+)
+
+CREATE TABLE `competition` (
+    `id` varchar(255) NOT NULL,
+    `tenant_id` bigint NOT NULL,
+    `title` text NOT NULL,
+    `finished_at` bigint DEFAULT NULL,
+    `created_at` bigint NOT NULL,
+    `updated_at` bigint NOT NULL,
+    KEY `idx_competition_tenant_id` (`tenant_id`)
+)
+
+INSERT INTO player SELECT * FROM player_init;
+INSERT INTO player_score SELECT * FROM player_score_init;
+INSERT INTO competition SELECT * FROM competition_init;
